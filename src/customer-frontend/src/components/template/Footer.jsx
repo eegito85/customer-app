a654f2ac67f934b3110e872de65fdd5e3c8b7201
Emmanuel Egito Oliveira Candeira Marinho <e_egito@hotmail.com>
2023-11-17 09:58:31 -0300

    create the main layout

---
+++ src/customer-frontend/src/components/template/Footer.jsx
@@ -0,0 +1,7 @@
+import './Footer.css'
+import React from 'react'
+
+export default props => 
+<footer className="footer">
+    <span>Desenvolvido pro <strong> Emmanuel Egito</strong></span>
+</footer>
